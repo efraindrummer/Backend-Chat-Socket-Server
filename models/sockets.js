f3f8a3958e2a7f6e51aeff9645216b9c02460769
Efrain May <efrain_may_75@hotmail.com>
2021-01-03 20:58:06 -0600

    TODOS: listos, backend realizado basicamente

--- models/sockets.js
+++ models/sockets.js
@@ -12,15 +12,14 @@ class Sockets {
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
+            //validar el jwt en el instancte y si es valido continua
+            //si el token no es valido se tendra que desconectar
+            //todo el usuario tiene que saber que esta activo y se tiene ue hacer hace mediante el uid
+            //emitir todos los usarios conectados
+            //socket join - para unirse a una sala especifica con el mismo uid
+            //escuchar cuando el cliente manda un mensaje - Mensaje personal
+            //disconnect para marcar en la base de datos que es usuario se desconecto
+            //emitir todos los usaurios conectados jejejeje, disfrutar trabajando JAJAJAJ  (:
         });
     }
 
