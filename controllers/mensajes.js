62b9ca8d75fa3a8009815b7c6a67d1fcc25093ae
Efrain May <efrain_may_75@hotmail.com>
2021-01-03 20:52:13 -0600

    obtener los mensajes del chat

---
+++ controllers/mensajes.js
@@ -0,0 +1,24 @@
+const Mensaje = require('../models/mensaje');
+
+const obtenerChat = async(req, res) => {
+    const midId = req.uid;
+    const mensajesDe = req.params.de;
+
+    const last30 = await Mensaje.find({
+        $or: [
+            {de: midId, para: mensajesDe},
+            {de: mensajesDe, para: midId},
+        ]
+    })
+    .sort({ createdAt: 'desc'})
+    .limit(30);
+
+    res.json({
+        ok: true,
+        mensajesDe: last30
+    });
+}
+
+module.exports = {
+    obtenerChat
+}
