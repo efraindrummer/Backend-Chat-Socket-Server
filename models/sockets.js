b5c45ccbcb8eb79dc17e8d46b07d2b5d28e24a72
Efrain May <efrain_may_75@hotmail.com>
2021-01-04 21:05:37 -0600

    identifcacion de la persona que se conecta y la que se desconecta obteniendo en id

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,4 @@
+const { comprobarJWT } = require("../helpers/jwt");
 
 
 class Sockets {
@@ -12,6 +13,14 @@ class Sockets {
     socketEvents() {
         // On connection
         this.io.on('connection', ( socket ) => {
+            
+            const [ valido, uid ] = comprobarJWT(socket.handshake.query['x-token']);
+
+            if(!valido){
+                console.log('Socket no identificado');
+                return socket.disconnect();
+            }
+            console.log('cliente conectado: ', uid);
             //validar el jwt en el instancte y si es valido continua
             //si el token no es valido se tendra que desconectar
             //todo el usuario tiene que saber que esta activo y se tiene ue hacer hace mediante el uid
@@ -20,6 +29,9 @@ class Sockets {
             //escuchar cuando el cliente manda un mensaje - Mensaje personal
             //disconnect para marcar en la base de datos que es usuario se desconecto
             //emitir todos los usaurios conectados jejejeje, disfrutar trabajando JAJAJAJ  (:
+            socket.on('disconnect', () => {
+                console.log('cliente desconectado', uid);
+            })
         });
     }
 
